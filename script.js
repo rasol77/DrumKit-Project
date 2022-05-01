56146a714df58fb7de4a6df1d2357eb303ad41f9
Carlos <58401176+rasol77@users.noreply.github.com>
2022-05-01 15:22:50 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,84 @@
 'use strict';
 //Seleccionamos del HTML las etiquetas de audio & las teclas
 
-const splash = document.querySelectorAll('audio');
-const divs = document.querySelectorAll('.drum-kit div');
+//const splash = document.querySelectorAll('audio');
+const divs = document.querySelectorAll('.key').length;
 
-window.addEventListener('keydown', (element) => {
-  //Codigo del teclado
-  let codes = element.keyCode.toString();
+for (let i = 0; i < divs; i++) {
+  document.querySelectorAll('.key')[i].addEventListener('click', function () {
+    const buttonInnerHTML = this.innerHTML;
 
-  //Reproducción del  código  si la tecla es igual al audio
-  splash.forEach((noise) => {
-    if (codes === noise.id) {
-      noise.play();
-      noise.currentTime = 0;
-    }
+    makeSound(buttonInnerHTML);
+
+    buttonAnimation(buttonInnerHTML);
   });
+}
 
-  //Añadir animaciones para los div`s
-  divs.forEach((div) => {
-    if (codes === div.id) {
-      //lo vinculamos a la animación con el CSS
-      div.classList.add('animate');
-      setTimeout(() => div.classList.remove('animate'), 100);
-    }
-  });
+//Vinculamos el botón con el sonido
+document.addEventListener('keypress', function (event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
 });
+
+//Creamos un Switch para vincular la tecla con el sonido correspondiente
+function makeSound(key) {
+  switch (key) {
+    case 'e':
+      const crash = new Audio('audio/crash.wav');
+      crash.play();
+      break;
+
+    case 'w':
+      const hithatClose = new Audio('audio/hihat-close.wav');
+      hithatClose.play();
+      break;
+
+    case 'a':
+      const hithatOpen = new Audio('audio/hihat-open.wav');
+      hithatOpen.play();
+      break;
+
+    case 's':
+      const kick = new Audio('audio/kick.wav');
+      kick.play();
+      break;
+
+    case 'd':
+      const snare = new Audio('audio/snare.wav');
+      snare.play();
+      break;
+
+    case 'j':
+      const tomHigh = new Audio('audio/tom-high.wav');
+      tomHigh.play();
+      break;
+
+    case 'k':
+      const tomLow = new Audio('audio/tom-low.wav');
+      tomLow.play();
+      break;
+
+    case 'l':
+      const tomMid = new Audio('audio/tom-mid.wav');
+      tomMid.play();
+      break;
+
+    case 'i':
+      const ride = new Audio('audio/ride.wav');
+      ride.play();
+      break;
+  }
+}
+
+//Añadir animaciones para las teclas
+
+function buttonAnimation(currentKey) {
+  const aniButton = document.querySelector('.' + currentKey);
+
+  aniButton.classList.add('animate');
+
+  //tiempo de la animacción
+  setTimeout(function () {
+    aniButton.classList.remove('animate');
+  }, 100);
+}
